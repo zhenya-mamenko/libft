1db1d0e8e8f61bdbdbd6437497beb1f906f55b94
Evgenii Mamenko <mamenko@gmail.com>
2019-02-14 13:43:41 -0800

    ft_strncmp fix

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emamenko <emamenko@student.42.us.org>      +#+  +:+       +#+        */
+/*   By: emamenko <emamenko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/11 15:28:36 by emamenko          #+#    #+#             */
-/*   Updated: 2019/02/11 16:25:22 by emamenko         ###   ########.fr       */
+/*   Updated: 2019/02/14 13:43:14 by emamenko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 			return (s1[i] - s2[i]);
 		i += 1;
 	}
+	if (i == n)
+		return (0);
 	if (s1[i] == '\0')
 	{
 		if (s2[i] == '\0')

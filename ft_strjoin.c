66293d2c6eecdfb80b93b6dd1715782a9e4e5293
Evgenii Mamenko <mamenko@gmail.com>
2019-02-16 12:29:05 -0800

    strjoin fix

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: emamenko <emamenko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/13 08:59:22 by emamenko          #+#    #+#             */
-/*   Updated: 2019/02/16 12:24:01 by emamenko         ###   ########.fr       */
+/*   Updated: 2019/02/16 12:28:50 by emamenko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char			*ft_strjoin(char const *s1, char const *s2)
 	char	*b;
 
 	if (s1 == NULL || s2 == NULL)
-		return (s1 == NULL ? s2 : s1);
+		return ((char *)(s1 == NULL ? s2 : s1));
 	b = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (b != NULL)
 	{

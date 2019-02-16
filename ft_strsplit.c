7e213d64c95dfad5fa0b000f8cc5d91839cdeb7e
Evgenii Mamenko <mamenko@gmail.com>
2019-02-16 14:20:41 -0800

    strsplit malloc check

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: emamenko <emamenko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/14 16:10:48 by emamenko          #+#    #+#             */
-/*   Updated: 2019/02/16 13:52:38 by emamenko         ###   ########.fr       */
+/*   Updated: 2019/02/16 14:20:21 by emamenko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,8 @@ char	**ft_strsplit(char const *s, char c)
 		return (NULL);
 	count = ft_word_count(s, c);
 	a = malloc(sizeof(char *) * (count + 1));
+	if (a == NULL)
+		return (NULL);
 	a[count] = 0;
 	i = 0;
 	while (i < count)

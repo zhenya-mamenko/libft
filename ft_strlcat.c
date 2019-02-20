616a3984e5f28cb0d8f1bc3a3c6d4669ded87a81
Evgenii Mamenko <mamenko@gmail.com>
2019-02-19 16:47:02 -0800

    ft_strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: emamenko <emamenko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/11 14:57:32 by emamenko          #+#    #+#             */
-/*   Updated: 2019/02/15 22:31:53 by emamenko         ###   ########.fr       */
+/*   Updated: 2019/02/19 16:46:50 by emamenko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,25 +19,18 @@ size_t			ft_strlcat(char *dst, const char *src, size_t dstsize)
 	size_t	c;
 	size_t	ld;
 
-	i = 0;
-	c = 0;
 	ld = ft_strlen(dst);
-	if (dstsize != 0)
-		while (i < (dstsize - 1))
-		{
-			if (c > 0 || !dst[i])
-			{
-				dst[i] = src[c];
-				if (!src[c])
-					break ;
-				c += 1;
-			}
-			i += 1;
-		}
-	if (dstsize >= ld)
+	i = ld;
+	c = 0;
+	while (i < (dstsize - 1))
 	{
-		dst[i] = '\0';
-		return (ld + ft_strlen(src));
+		dst[i] = src[c];
+		if (!src[c])
+			break ;
+		c += 1;
+		i += 1;
 	}
-	return (dstsize + ft_strlen(src));
+	if (dstsize >= ld && dstsize != 0)
+		dst[i] = '\0';
+	return (ld + ft_strlen(src));
 }
